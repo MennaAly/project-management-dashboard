d3e0645eb3063cd5854340ab3a29561086c8387a
MennaAly <mennaali365@gmail.com>
2024-11-22 19:12:06 +0200

    add spinner component

---
+++ src/UI/spinner/spinner.tsx
@@ -0,0 +1,11 @@
+import './spinner.css'; // Create this file for spinner styles
+
+function Spinner() {
+  return (
+    <div className="spinner">
+      <div className="spinner-circle"></div>
+    </div>
+  );
+}
+
+export default Spinner;
