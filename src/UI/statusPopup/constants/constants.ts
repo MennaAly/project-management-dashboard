8581b1bfc51ade8600775c9545c732c4206ec0c5
MennaAly <mennaali365@gmail.com>
2024-11-22 19:16:59 +0200

    add status popup constants

---
+++ src/UI/statusPopup/constants/constants.ts
@@ -0,0 +1,14 @@
+export const popupStatusProperties = {
+    loading: {
+        message: 'Saving...',
+        className: 'status-popup--loading'
+    },
+    error: {
+        message: 'Error',
+        className: 'status-popup--error'
+    },
+    success: {
+        message: 'Saved',
+        className: 'status-popup--success'
+    },
+} 
