ced3a71ce2e2af3d4ffe34426dc117dcfe7771ae
MennaAly <mennaali365@gmail.com>
2024-11-22 19:13:27 +0200

    add UI interface

---
+++ src/interfaces/UI.ts
@@ -0,0 +1,17 @@
+import { popupStaus } from "../types/UItypes";
+
+export interface ModalProps {
+    controllerBtnTitle: string,
+    title: string,
+    children: React.ReactNode, 
+    showSubmit: boolean,
+    submit: () => void,
+    modalTitleClass?: string; 
+    isLoading?: boolean;
+    isSuccess?: boolean;
+}
+
+export interface IStatusPopupProps {
+    status: popupStaus,
+    message?: string
+}
