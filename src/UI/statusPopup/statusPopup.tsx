48ad43fdfae581b7597e73cee690937c56bb3862
MennaAly <mennaali365@gmail.com>
2024-11-23 13:51:51 +0200

    show status popup in a portal to be above all the elements

--- src/UI/statusPopup/statusPopup.tsx
+++ src/UI/statusPopup/statusPopup.tsx
@@ -1,13 +1,14 @@
+import { createPortal } from 'react-dom';
 import { IStatusPopupProps } from "../../interfaces/UI";
 import { popupStatusProperties } from "./constants/constants";
 
 function StatusPopup({status, message} : IStatusPopupProps) {
-    const currentStatus = popupStatusProperties[status];
-    return(
+    const currentStatus = popupStatusProperties[status];    
+
+    return createPortal((
         <section className={`status-popup ${currentStatus.className}`}>
             {message ? message: currentStatus.message}
         </section>
-    )
+    ), document.getElementById('status-popup') as HTMLElement)
 }
-
 export default StatusPopup;
